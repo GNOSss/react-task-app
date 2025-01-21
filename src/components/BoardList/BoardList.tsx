d7d3e34a10539339f9d323aad52a3403bd26766a
SONGSEUNGSOO <sssong1993@gmail.com>
2025-01-21 23:09:50 +0900

    250121

--- src/components/BoardList/BoardList.tsx
+++ src/components/BoardList/BoardList.tsx
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { FC, useState } from 'react';
+import { useTypedSelector } from '../../hooks/redux';
+import SideForm from './SideForm/SideForm';
+import { FiPlusCircle } from 'react-icons/fi';
+import { addButton, addSection, boardItemActive, container, title, boardItem } from './BoardList.css';
+import clsx from 'clsx';
+import { useRef } from 'react';
 
-const BoardList = () => {
-  return <div></div>;
+type TBoardListProps = {
+  activeBoardId: string;
+  setActiveBoardId: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const BoardList: FC<TBoardListProps> = ({ activeBoardId, setActiveBoardId }) => {
+  const { boardArray } = useTypedSelector((state) => state.boards);
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    setIsFormOpen(!isFormOpen);
+    setTimeout(() => {
+      inputRef.current?.focus();
+    }, 0);
+  };
+
+  return (
+    <div className={container}>
+      <div className={title}>게시판 :</div>
+
+      {boardArray.map((board, index) => (
+        <div
+          key={board.boardId}
+          onClick={() => setActiveBoardId(boardArray[index].boardId)}
+          className={clsx(
+            {
+              [boardItemActive]: boardArray.findIndex((board) => board.boardId === activeBoardId) === index,
+            },
+            {
+              [boardItem]: boardArray.findIndex((b) => b.boardId === activeBoardId) !== index,
+            }
+          )}
+        >
+          <div>{board.boardName}</div>
+        </div>
+      ))}
+      <div className={addSection}>
+        {isFormOpen ? <SideForm inputRef={inputRef} setIsFormOpen={setIsFormOpen} /> : <FiPlusCircle className={addButton} onClick={handleClick} />}
+      </div>
+    </div>
+  );
 };
 
 export default BoardList;
--- src/components/ListsContainer/ListsContainer.tsx
+++ src/components/ListsContainer/ListsContainer.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { IList } from '../../types';
+import List from '../List/List';
+import ActionButton from '../ActionButton/ActionButton';
+import { listsContainer } from './ListsContainer.css';
 
-const ListsContainer = () => {
-  return <div></div>;
+type TListsContainerProps = {
+  boardId: string;
+  lists: IList[];
+};
+
+const ListsContainer: FC<TListsContainerProps> = ({ lists, boardId }) => {
+  return (
+    <div className={listsContainer}>
+      {lists.map((list) => (
+        <List
+          key={list.listId}
+          list={list}
+          boardId={boardId} // pass boardId to List component for task filtering
+        />
+      ))}
+      <ActionButton boardId={boardId} listId={''} list />
+    </div>
+  );
 };
 
 export default ListsContainer;
