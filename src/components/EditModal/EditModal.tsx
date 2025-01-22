26e08eb37b5d2d4baa3a130b0b1f56759059376c
SONGSEUNGSOO <sssong1993@gmail.com>
2025-01-22 15:12:32 +0900

    add Modal, LogItem, deleteBoard

--- src/components/EditModal/EditModal.tsx
+++ src/components/EditModal/EditModal.tsx
@@ -1,7 +1,106 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
+import { FiX } from 'react-icons/fi';
+import { useTypedDispatch, useTypedSelector } from '../../hooks/redux';
+import { useState } from 'react';
+import { deleteTask, setModalActive, updateTask } from '../../store/slices/boardsSlice';
+import { addLog } from '../../store/slices/loggerSlice';
+import { v4 } from 'uuid';
+import { buttons, closeButton, deleteButton, header, input, modalWindow, title, updateButton, wrapper } from './EditModal.css';
 
 const EditModal = () => {
-  return <div></div>;
+  const dispatch = useTypedDispatch();
+
+  const editingState = useTypedSelector((state) => state.modal);
+
+  const [data, setData] = useState(editingState);
+
+  const handleCloseButton = () => {
+    dispatch(setModalActive(false));
+  };
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setData({
+      ...data,
+      task: { ...data.task, taskName: e.target.value },
+    });
+  };
+
+  const handleDescriptionChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setData({
+      ...data,
+      task: { ...data.task, taskDescription: e.target.value },
+    });
+  };
+
+  const handleAuthorChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setData({
+      ...data,
+      task: { ...data.task, taskOwner: e.target.value },
+    });
+  };
+
+  const handleUpdate = () => {
+    dispatch(
+      updateTask({
+        boardId: editingState.boardId,
+        listId: editingState.listId,
+        task: data.task,
+      })
+    );
+    dispatch(
+      addLog({
+        logId: v4(),
+        logMessage: `일 수정 ${editingState.task.taskName} `,
+        logAuthor: 'User',
+        logTimestamp: String(Date.now()),
+      })
+    );
+    dispatch(setModalActive(false));
+  };
+
+  const handleDelete = () => {
+    dispatch(
+      deleteTask({
+        boardId: editingState.boardId,
+        listId: editingState.listId,
+        taskId: editingState.task.taskId,
+      })
+    );
+    dispatch(
+      addLog({
+        logId: v4(),
+        logMessage: `일 삭제 ${editingState.task.taskName} `,
+        logAuthor: 'User',
+        logTimestamp: String(Date.now()),
+      })
+    );
+    dispatch(setModalActive(false));
+  };
+
+  return (
+    <div className={wrapper}>
+      <div className={modalWindow}>
+        <div className={header}>
+          <div className={title}>{editingState.task.taskName}</div>
+          <FiX className={closeButton} onClick={handleCloseButton} />
+        </div>
+        <div className={title}>제목</div>
+        <input className={input} type="text" value={data.task.taskName} onChange={handleNameChange} />
+        <div className={title}>설명</div>
+        <input className={input} type="text" value={data.task.taskDescription} onChange={handleDescriptionChange} />
+        <div className={title}>생성자</div>
+        <input className={input} type="text" value={data.task.taskOwner} onChange={handleAuthorChange} />
+        <div className={buttons}>
+          <button onClick={handleUpdate} className={updateButton}>
+            수정
+          </button>
+          <button onClick={handleDelete} className={deleteButton}>
+            삭제
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EditModal;
--- src/components/LoggerModal/LogItem/LogItem.tsx
+++ src/components/LoggerModal/LogItem/LogItem.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { ILogItem } from '../../../types';
+import { BsFillPersonFill } from 'react-icons/bs';
+import { author, date, logItemWrap, message } from './LogItem.css';
 
-const LogItem = () => {
-  return <div></div>;
+type TLogItemProps = {
+  logItem: ILogItem;
+};
+
+const LogItem: FC<TLogItemProps> = ({ logItem }) => {
+  let timeOffset = new Date(Date.now() - Number(logItem.logTimestamp));
+
+  const showOffsetTime = `
+  ${timeOffset.getMinutes() > 0 ? `${timeOffset.getMinutes()}분` : ''}
+  ${timeOffset.getSeconds() > 0 ? `${timeOffset.getSeconds()}초 전` : ''}
+  ${timeOffset.getSeconds() === 0 ? '방금 전' : ''}
+  `;
+
+  return (
+    <div className={logItemWrap}>
+      <div className={author}>
+        <BsFillPersonFill />
+        {logItem.logAuthor}
+      </div>
+      <div className={message}>{logItem.logMessage}</div>
+      <div className={date}>{showOffsetTime}</div>
+    </div>
+  );
 };
 
 export default LogItem;
